672160f2ffb6f37e402ca2018f2a0b54134ce23f
Adnane <biouiadnane@gmail.com>
2024-10-11 18:16:11 +0100

    first commit

---
+++ src/main/java/bankaccount/com/backend_bankaccount/BackendBankAccountApplication.java
@@ -0,0 +1,13 @@
+package bankaccount.com.backend_bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBankAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bankaccount/com/backend_bankaccount/BackendBankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package bankaccount.com.backend_bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
